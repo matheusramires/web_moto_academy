daa60180e809851ec450ed5c4ac69d76cc7e3e83
Matheus Ramires <souzaramiresdev@gmail.com>
2023-11-06 20:20:16 -0400

    dando erro

--- aula01/javascript.js
+++ aula01/javascript.js
@@ -1,10 +1,58 @@
-const Pessoa = {
-    nome: "Matheus",
-    idade: 26,
-    sexo: "M",
-    like : ["Estudar", "Caminhar", "Dormir"],
-    acao : function() { console.log(Pessoa["like"][1])}
+//const Pessoa = {
+//    nome: "Matheus",
+//    idade: 26,
+//    sexo: "M",
+//    like : ["Estudar", "Caminhar", "Dormir"],
+//    acao : function() { console.log(Pessoa["like"][1])}
+//}
+
+//Pessoa.acao()
+
+function somar(valor){
+    return valor + 10;
 }
 
-Pessoa.acao()
 
+
+
+
+
+//teste = (a) => 2023-a;
+//console.log(teste(26));
+
+//teste = (a) => 2023-a;
+//console.log(teste(1997));
+
+
+let array = [
+    {
+      id: 1,
+      name: 'John',
+      age: 23,
+    },
+    {
+      id: 2,
+      name: 'Samuel',
+      age: 21,
+    },
+    {
+      id: 3,
+      name: 'marvin',
+      age: 26,
+    },
+    {
+      id: 4,
+      name: 'james',
+      age: 28,
+    },
+  ];
+
+const resultado  = array.map((resposta) => {
+    if (resposta.name == 'John'){
+        resposta.idade = reposta.age + 10;
+    }
+    return '';
+
+});
+
+console.log(resultado)
